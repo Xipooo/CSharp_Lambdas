527dbb26b1fee1471b5df72c4c262c5ec35b0b35
Steve Bishop <xipooo@gmail.com>
2018-06-23 15:15:36 -0700

    Switched to Custom Delegate

--- MathService.cs
+++ MathService.cs
@@ -5,12 +5,15 @@ namespace CSharp_Lambdas
 {
     public class MathService
     {
-        public event EventHandler<MathPerformedEventArgs> MathPerformed;
+        //public event EventHandler<MathPerformedEventArgs> MathPerformed;
+        public delegate void MathPerformedHandler(double result);
 
+        public event MathPerformedHandler MathPerformed;
         public void MultiplyNumbers(double value1, double value2)
         {
             Timer timer = new Timer(5000);
-            MathPerformed(this, new MathPerformedEventArgs{ Result = value1 * value2});
+            //MathPerformed(this, new MathPerformedEventArgs{ Result = value1 * value2});
+            MathPerformed(value1 + value2);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace CSharp_Lambdas
         static void Main(string[] args)
         {
             MathService mathServ = new MathService();
-            mathServ.MathPerformed += (sender, e) => Console.WriteLine("Caluculation Result: " + e.Result);
+            //mathServ.MathPerformed += (sender, e) => Console.WriteLine("Caluculation Result: " + e.Result);
+            mathServ.MathPerformed += (result) => Console.WriteLine("Calculated Result: " + result);
 
             mathServ.MultiplyNumbers(43.598, 87.23);
         }
