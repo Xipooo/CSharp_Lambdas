aee8d551bc8be40bff65a8fb30f8193525bbbb1b
Steve Bishop <xipooo@gmail.com>
2018-06-23 15:04:46 -0700

    Converted Anonymous Function to Lambda

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace CSharp_Lambdas
         static void Main(string[] args)
         {
             MathService mathServ = new MathService();
-            mathServ.MathPerformed += delegate(object sender, MathPerformedEventArgs e) {
-                Console.WriteLine("Caluculation Result: " + e.Result);
-            };
+            mathServ.MathPerformed += (sender, e) => Console.WriteLine("Caluculation Result: " + e.Result);
 
             mathServ.MultiplyNumbers(43.598, 87.23);
         }
