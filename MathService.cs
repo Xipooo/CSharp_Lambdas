86d5a15b4b0783665775f901e83a78a64f437e1f
Steve Bishop <xipooo@gmail.com>
2018-06-23 15:22:06 -0700

    Passed Delegate as parameter

--- MathService.cs
+++ MathService.cs
@@ -7,13 +7,13 @@ namespace CSharp_Lambdas
     {
         //public event EventHandler<MathPerformedEventArgs> MathPerformed;
         public delegate void MathPerformedHandler(double result);
-
+        public delegate double CalculationHandler(double value1, double value2);
         public event MathPerformedHandler MathPerformed;
-        public void MultiplyNumbers(double value1, double value2)
+        public void CalculateNumbers(double value1, double value2, CalculationHandler calculation)
         {
             Timer timer = new Timer(5000);
             //MathPerformed(this, new MathPerformedEventArgs{ Result = value1 * value2});
-            MathPerformed(value1 + value2);
+            MathPerformed(calculation(value1, value2));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CSharp_Lambdas
             //mathServ.MathPerformed += (sender, e) => Console.WriteLine("Caluculation Result: " + e.Result);
             mathServ.MathPerformed += (result) => Console.WriteLine("Calculated Result: " + result);
 
-            mathServ.MultiplyNumbers(43.598, 87.23);
+            mathServ.CalculateNumbers(43.598, 87.23, (value1, value2) => value1 * value2);
         }
     }
 }
